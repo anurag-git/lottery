248d8be74a7aea8da8f20833a26805a4b8ebf134
Anurag Dashputre <anurag4u80@gmail.com>
2018-03-30 17:02:29 +0530

    First commit

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+   'glass program people crew pen neither birth addict train section board plunge',
+   'https://rinkeby.infura.io/weUq77E9u1GHJiRfQ9eB'
+);
+
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('Attempting to deploy from account', accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+	.deploy({ data: bytecode })
+	.send({ gas: '1000000',from: accounts[0]});
+
+	console.log(interface);
+	console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
