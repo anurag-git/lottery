9f66e3e2f3b88f67b3bf3e57050d9148c0111bbb
Anurag Dashputre <anurag4u80@gmail.com>
2018-05-29 23:37:07 +0530

    Added few new functions
    Added getFinalBalance(), getAllBalances(), getNoOfPlayers(), getBalance().
    Updated pickWinner() to caculate finalBalance and return address of winner.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,10 +1,12 @@
-pragma solidity ^0.4.17; 
+pragma solidity ^0.4.24;
 
 contract Lottery {
     address public manager;
     address[] public players;
+    
+    uint[] private finalBalance;
 
-    function Lottery() public {
+    constructor() public {
         manager = msg.sender;
     }
 
@@ -13,22 +15,47 @@ contract Lottery {
         players.push(msg.sender);
     }
 
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty,now,players));
-    }
-
-    function pickWinner() public restricted {
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
-    }
-
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
+    
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+    }
+
+    function pickWinner() public restricted returns(address){
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        finalBalance = getAllBalances();
+        address[] memory tempPlayers = new address[](index);
+        tempPlayers = getPlayers();
+        players = new address[](0);
+        return tempPlayers[index]; // return address of winner
+    }
 
     function getPlayers() public view returns (address[]) {
         return players;
+    }
+    
+    function getBalance(address _addr) public view returns (uint) {
+        return _addr.balance;
+    }
+    
+    function getNoOfPlayers() public view returns (uint) {
+        return players.length;
+    }
+    
+    function getAllBalances() public view returns (uint[]) {
+        uint[] memory allBalances = new uint[](players.length);
+        for(uint i=0; i<players.length; i++) {
+            allBalances[i] = players[i].balance;
+        }
+        
+        return allBalances;
+    }
+    
+    function getFinalBalance() public view returns (uint[]) {
+        return finalBalance;
     }
 }
